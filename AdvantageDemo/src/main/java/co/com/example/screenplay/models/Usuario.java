b060a1d4113ab97f1144c60259331eb6211bcc1e
John Jaime Soto Moncada <jojsoto@bancolombia.com.co>
2022-05-29 20:10:38 -0500

    cambio variables

--- AdvantageDemo/src/main/java/co/com/example/screenplay/models/Usuario.java
+++ AdvantageDemo/src/main/java/co/com/example/screenplay/models/Usuario.java
@@ -5,8 +5,6 @@ import lombok.Data;
 @Data
 public class Usuario {
 
-
-
     private String username;
     private String  email;
     private String password;
@@ -20,19 +18,4 @@ public class Usuario {
     private String  state;
     private String postal_code;
 
-
-    public Usuario(String username, String email, String password, String confirm_password, String first_name, String last_name, String phone_number, String country, String city, String address, String state, String postal_code) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.confirm_password = confirm_password;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.phone_number = phone_number;
-        this.country = country;
-        this.city = city;
-        this.address = address;
-        this.state = state;
-        this.postal_code = postal_code;
-    }
 }
